b4a6b4283923f3473961d74a73451a83c9814714
Hugo Donato <hdonato@servicoop.com>
2024-07-23 08:12:12 -0300

    quitando del rastreador archivos autogenerados

--- src/reporte/index.js
+++ src/reporte/index.js
@@ -10,10 +10,9 @@ RenderHTML.prototype.renderizar = function () {
     const archivoHTML = fs.readFileSync('./web/public/index.html', 'utf8');
     // Carga el contenido en Cheerio
     const $ = cheerio.load(archivoHTML);
-    // Selecciona el elemento con id 'contenido' y obtén su innerHTML
-    const contenidoHTML = $('#contenido').html();
-
-    console.log(contenidoHTML);    
+    
+    const contenidoBody = $('body').html();
+    console.log(contenidoBody);
 }
 
 module.exports = RenderHTML;

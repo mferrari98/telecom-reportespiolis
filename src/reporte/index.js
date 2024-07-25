561d2fa1c4e164c2af6b546ef169f433cb5061b4
Hugo Donato <hdonato@servicoop.com>
2024-07-25 12:56:18 -0300

    terminando el cierre del ciclo entre la generacion de un nuevo archivo plano y la generacion del email correspondiente

--- src/reporte/index.js
+++ src/reporte/index.js
@@ -1,8 +1,32 @@
 const fs = require('fs');
 const cheerio = require('cheerio');
 const puppeteer = require('puppeteer');
+const nodemailer = require('nodemailer');
 
 const ID_MOD = "Render"
+
+/*
+==================================================
+============ pasar a un modulo aparte ============
+==================================================
+ */
+// Configuración del transporte SMTP
+let transporter = nodemailer.createTransport({
+    host: 'post.servicoop.com',     // Cambia esto por el servidor SMTP que estés usando
+    port: 25,                       // Usualmente 587 o 465 para SSL    
+    auth: {
+        user: 'hdonato@servicoop.com',  // Cambia esto por tu correo electrónico
+        pass: 'donato'                  // Cambia esto por tu contraseña
+    },
+    tls: {
+        rejectUnauthorized: false       // omitir verificacion en cadena
+    }
+});
+/*
+==================================================
+============ pasar a un modulo aparte ============
+==================================================
+ */
 
 function RenderHTML() { }
 
@@ -10,7 +34,38 @@ RenderHTML.prototype.renderizar = function () {
     
     const archivoHTML = fs.readFileSync('./web/public/index.html', 'utf8');
     extraerTabla(archivoHTML)
-    ejecutarPlotImagen()
+    ejecutarPlotImagen(() => {
+
+        // Enviar el correo
+        let resumen = "generacion automatica de reportes mejorada"
+        let htmlContent = fs.readFileSync('./reporte/salida/tabla.html', 'utf8');
+
+        let mailOptions = {
+            from: "'soyhugo' <hdonato@servicoop.com>",
+            to: 'hdonato@servicoop.com',
+            subject: 'reportespiolis',
+            text: resumen,
+            html: `
+            ${resumen}
+            ${htmlContent}
+            <img src="cid:graficobarras"/>
+            `,
+            attachments: [                
+                {
+                    filename: 'imagen.jpg',
+                    path: './reporte/salida/grafico.png', // Ruta de la imagen
+                    cid: 'graficobarras' // CID para referenciar la imagen en el cuerpo del mensaje
+                }
+            ]
+        }
+
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                return console.log(error);
+            }
+            console.log('Message sent: %s', info.messageId);            
+        });
+    })
 }
 
 function extraerTabla(archivoHTML) {
@@ -36,7 +91,7 @@ function extraerTabla(archivoHTML) {
     fs.writeFileSync('./reporte/salida/tabla.html', newHtml, 'utf8');
 }
 
-async function ejecutarPlotImagen() {
+async function ejecutarPlotImagen(cb) {
     const browser = await puppeteer.launch();
     const page = await browser.newPage();
     await page.goto(`file://${process.cwd()}/web/public/index.html`);
@@ -46,6 +101,7 @@ async function ejecutarPlotImagen() {
     await element.screenshot({ path: './reporte/salida/grafico.png' });
 
     await browser.close();
+    cb()
 }
 
 module.exports = RenderHTML;
--- src/web/server.js
+++ src/web/server.js
@@ -10,13 +10,11 @@ app.use(express.json());
 
 /*
 Middleware para configurar Content-Security-Policy
-
-esta regla parece que no hace falta
-script-src 'self' 'unsafe-inline' 'unsafe-eval' http://localhost:3000;\
 */
 app.use((req, res, next) => {
   res.setHeader("Content-Security-Policy", "\
     default-src 'none' http://localhost:3000/;\
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' http://localhost:3000;\
     style-src 'self' 'unsafe-inline' http://localhost:3000;\
     img-src 'self' data: blob: http://localhost:3000;\
     connect-src 'self' http://localhost:3000"

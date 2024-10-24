4d2344716ee9a450cd0f8db990f22643e391b181
Hugo Donato <hdonato@servicoop.com>
2024-10-24 11:34:29 -0300

    convirtiendo a server web seguro

--- index.js
+++ index.js
@@ -31,3 +31,4 @@ process.on('SIGINT', () => {
 });
 
 logamarillo(1, `${ID_MOD} - Directorio trabajo:`, process.cwd());
+logamarillo(1, `${ID_MOD} - Directorio del archivo:`, __dirname);
--- src/web/server.js
+++ src/web/server.js
@@ -1,55 +1,49 @@
-/*
-lo podes probar con
-  curl -Uri "http://10.10.4.125:3000/sitio" -Method POST -ContentType "application/json" -Body '{"descriptor":"ee3"}'
-*/
 const express = require('express');
 const path = require('path');
+const https = require('https');
+const fs = require('fs');
+const { activo } = require("../../config.json").desarrollo;
+const { logamarillo } = require("../control/controlLog");
 
-const { activo } = require("../../config.json").desarrollo
-const { logamarillo } = require("../control/controlLog")
-
-const ID_MOD = "WEBSERV"
-
+const ID_MOD = "WEBSERV";
 const app = express();
 app.use(express.json());
 
-let currentPort = (activo) ? 3001 : 3000;        // Puerto inicial
+let currentPort = (activo) ? 3001 : 3000;  // Puerto inicial
 
-/*
-Middleware para configurar Content-Security-Policy
-*/
+// Opciones HTTPS
+const httpsOptions = {
+  key: fs.readFileSync('./src/web/cert/server.key'),
+  cert: fs.readFileSync('./src/web/cert/server.crt'),
+  passphrase: 'escadadiamasdificil',
+};
+
+// Middleware para Content-Security-Policy
 app.use((req, res, next) => {
   res.setHeader("Content-Security-Policy", "\
-    script-src 'self' 'unsafe-inline' 'unsafe-eval' http://localhost:" + currentPort + ";\
-    style-src 'self' 'unsafe-inline' http://localhost:" + currentPort + ";\
-    img-src 'self' data: blob: http://localhost:" + currentPort + ";\
-    connect-src 'self' http://localhost:" + currentPort + ";\
-    "
-  );
+    script-src 'self' 'unsafe-inline' 'unsafe-eval' https://localhost:" + currentPort + ";\
+    style-src 'self' 'unsafe-inline' https://localhost:" + currentPort + ";\
+    img-src 'self' data: blob: https://localhost:" + currentPort + ";\
+    connect-src 'self' https://localhost:" + currentPort + ";\
+  ");
   next();
 });
 
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.json({ limit: '10mb' }));
-/*
-alta de rutas para organizar mejor el codigo
-*/
+
+// Importar rutas
 const sitioRoutes = require('./routes/sitio');
 const tipoVarRoutes = require('./routes/tipovar');
 const generalRoutes = require('./routes/general');
 const desarrolloRoutes = require('./routes/desarrollo');
 
-/*
-levantar server
-*/
-const server = app.listen(currentPort, () => {
-  logamarillo(2, `${ID_MOD} - Escuchando en p=${currentPort}`);
+// Crear el servidor HTTPS
+const server = https.createServer(httpsOptions, app).listen(currentPort, () => {
+  logamarillo(2, `${ID_MOD} - Escuchando en p=${currentPort} (HTTPS)`);
 });
 
-/*
-gestionar conexiones abiertas
-para poder hacer un cierre autoritario cuando sea necesario
-*/
+// Gestionar conexiones abiertas
 const connections = new Set();
 
 server.on('connection', (conn) => {
@@ -61,15 +55,15 @@ server.on('connection', (conn) => {
 
 server.on('error', (conn) => {
   logamarillo(1, `${ID_MOD} - Error abriendo puerto`);
-})
+});
 
+// Función para cerrar el servidor
 function closeServer(cb) {
 
   logamarillo(1, `${ID_MOD} - destruyendo ${connections.size} conexion`);
   for (const conn of connections) {
     conn.destroy();
   }
-
   server.close(() => {
     logamarillo(1, `${ID_MOD} - Server closed`);
     cb();
@@ -77,13 +71,11 @@ function closeServer(cb) {
 }
 
 module.exports = function (observador) {
-
   app.use('/sitio', sitioRoutes);
   app.use('/tipovar', tipoVarRoutes);
   app.use('/reporte', generalRoutes(observador));
   app.use('/desa', desarrolloRoutes);
 
-  // Retornar lo que quieras, por ejemplo, iniciar un servidor o cualquier lógica
   return {
     closeServer
   };
